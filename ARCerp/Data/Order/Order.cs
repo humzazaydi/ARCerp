a1febf4a4ffbd483d107a8a49f7e9e3f618b3bff
Syed Hamza Zaidi <syedmohammadhumzazaydi@gmail.com>
2024-06-05 18:40:14 +0500

    updated

--- ARCerp/Data/Order/Order.cs
+++ ARCerp/Data/Order/Order.cs
@@ -2,9 +2,16 @@ namespace ARCerp.Data.Order
 {
     public class Order
     {
-        public string OrderId { get; set; }
+        public int OrderId { get; set; }
         public int Quantity { get; set; }
         public string? OrderNumber { get; set; }
         public string? Description { get; set; }
+        public Order(int orderId, int quantity, string orderNumber, string description)
+        {
+            OrderId = orderId;
+            Quantity = quantity;
+            OrderNumber = orderNumber;
+            Description = description;
+        }
     }
 }
